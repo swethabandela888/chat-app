e6d5340398ed4029d96da91edce0a4a286effb7f
bswetha <bswetha@synamedia.com>
2025-10-28 13:08:40 +0530

    updated mongodb connector and dockerignore

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 class MongoDBConnector {
     constructor() {
         this.isConnected = false;
-        this.connection_string = process.env.MONGODB_URI; // || 'mongodb://localhost:27017/chatapp';  
+        this.connection_string = process.env.MONGODB_URI; 
+        this.retries = 0;
+        this.maxRetries = 5; 
+
+        mongoose.connection.on('connected', () => {
+            this.isConnected = true;
+            console.log('Mongoose connected to', this.connection_string);
+        });
+
+        mongoose.connection.on('error', (err) => {
+            this.isConnected = false;
+            console.error('Mongoose connection error:', err);
+        }); 
     }
 
     async connect() {
@@ -11,17 +24,18 @@ class MongoDBConnector {
             console.log('MongoDB is already connected');
             return;
         }
-        try{
-            await mongoose.connect(this.connection_string)
-            .then(() => { 
+        while (!this.isConnected && this.retries < this.maxRetries) {
+            try {
+                await mongoose.connect(this.connection_string)
                 console.info('Connected to MongoDB');
                 this.isConnected = true;
-            }) 
-            .catch(err => console.error('MongoDB connection error:', err));             
-        }catch (err){
-            console.error('Error connecting to MongoDB:', err);
+                this.retries = 0;
+            } catch (err) {
+                this.retries++;
+                console.error('MongoDB connection error:', err)
+                await new Promise(res => setTimeout(res, 5000));
+            }
         }
-
     }
     async disconnect() {
         if (!this.isConnected) {
@@ -30,12 +44,8 @@ class MongoDBConnector {
         }   
         try {
             await mongoose.disconnect()
-            .then(() => {
-                this.isConnected = false;
-                console.info('Disconnected from MongoDB');
-            })
-            .catch(err => console.error('Error during MongoDB disconnection:', err));
-            
+            this.isConnected = false;
+            console.info('Disconnected from MongoDB');
         } catch (err) {
             console.error('Error disconnecting from MongoDB:', err);
         }  
